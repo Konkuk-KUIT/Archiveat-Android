b0ab4ea1448da77928c6482476506094ccef48ac
BearYeWon <bear100152@gmail.com>
2026-01-05 01:27:06 +0900

    feat: HomeScreen 네비게이션 탭 및 탭 별 스크린 추가

---
+++ app/src/main/java/com/kuit/archiveatproject/presentation/home/screen/TabScreen/DeepDiveScreen.kt
@@ -0,0 +1,14 @@
+package com.kuit.archiveatproject.presentation.home.screen
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DeepDiveScreen(
+    modifier: Modifier = Modifier,
+){
+    Text(
+        text = "집중 탐구"
+    )
+}
