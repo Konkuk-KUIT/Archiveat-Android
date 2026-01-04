e87981015b86ba3565e3bbd531244f5c636849f0
AyoungSe0 <ayoungseo1005@gmail.com>
2026-01-04 21:18:00 +0900

    feat: 홈/공통 컴포넌트-TextTag 및 Color, Mapping 추가

--- app/src/main/java/com/kuit/archiveatproject/presentation/home/model/HomeContentCardUiModel.kt
+++ app/src/main/java/com/kuit/archiveatproject/presentation/home/model/HomeContentCardUiModel.kt
@@ -1,12 +1,13 @@
 package com.kuit.archiveatproject.presentation.home.model
 
+import com.kuit.archiveatproject.domain.model.HomeCardType
 import com.kuit.archiveatproject.domain.model.HomeTabType
 
 data class HomeContentCardUiModel(
     val archiveId: Long,
     val tabType: HomeTabType,
     val tabLabel: String,
-    val cardType: String,
+    val cardType: HomeCardType,
     val title: String,
     val thumbnailUrl: String
 )
