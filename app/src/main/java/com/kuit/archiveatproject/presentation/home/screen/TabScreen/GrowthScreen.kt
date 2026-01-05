ae0bb68ac5c45fa83746ea442c5800f8d64108ee
BearYeWon <bear100152@gmail.com>
2026-01-05 21:30:11 +0900

    feat: HomeScreen 탭 별 서브 메시지 컴포넌트 추가 및 main container 색상 변경

--- app/src/main/java/com/kuit/archiveatproject/presentation/home/screen/TabScreen/GrowthScreen.kt
+++ app/src/main/java/com/kuit/archiveatproject/presentation/home/screen/TabScreen/GrowthScreen.kt
@@ -1,14 +1,25 @@
 package com.kuit.archiveatproject.presentation.home.screen
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.kuit.archiveatproject.presentation.home.component.SubMessageComponent
+import com.kuit.archiveatproject.ui.theme.ArchiveatProjectTheme
 
 @Composable
 fun GrowthScreen(
     modifier: Modifier = Modifier,
 ){
-    Text(
-        text = "성장 한입"
-    )
+    Row(
+        modifier = modifier
+            .padding(start = 20.dp)
+    ) {
+        SubMessageComponent(
+            "잠깐의 틈을 채워줄, 현재의 관심사와 맞닿은 인사이트",
+            ArchiveatProjectTheme.colors.sub_1
+        )
+    }
 }
