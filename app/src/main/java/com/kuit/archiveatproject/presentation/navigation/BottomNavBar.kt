a584acd9837301e8166b313ec5306b1f1db2022f
BearYeWon <bear100152@gmail.com>
2026-01-05 13:43:13 +0900

    feat: 하단 네비게이션 바 그림자 추가

--- app/src/main/java/com/kuit/archiveatproject/presentation/navigation/BottomNavBar.kt
+++ app/src/main/java/com/kuit/archiveatproject/presentation/navigation/BottomNavBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,8 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.kuit.archiveatproject.core.util.noRippleClickable
@@ -34,13 +38,20 @@ fun BottomNavBar(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .clip(
-                RoundedCornerShape(
+            .shadow(
+                elevation = 0.5.dp,
+                shape = RoundedCornerShape(topStart = 30.dp, topEnd = 30.dp),
+                clip = true,
+                ambientColor = Color.Black.copy(alpha = 0.6f),
+                spotColor = Color.Black.copy(alpha = 0.9f)
+            )
+            .background(
+                color = Color.White,
+                shape = RoundedCornerShape(
                     topStart = 30.dp,
                     topEnd = 30.dp
                 )
             )
-            .background(Color.White)
     ) {
         Column {
 
@@ -87,4 +98,10 @@ fun BottomNavBar(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun BottomNaBarPreview(){
+    BottomNavBar(true, NavTab.entries, NavTab.Home, onItemSelected ={})
 }
