9523f4d91bbb53ff5a080d923cb66a77df19b669
AyoungSe0 <ayoungseo1005@gmail.com>
2026-01-13 01:24:43 +0900

    feat: 공통 컴포넌트 BackTopBar 추가

---
+++ app/src/main/java/com/kuit/archiveatproject/core/component/BackTopBar.kt
@@ -0,0 +1,79 @@
+package com.kuit.archiveatproject.core.component
+
+import android.R.attr.contentDescription
+import android.R.attr.navigationIcon
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kuit.archiveatproject.R
+import com.kuit.archiveatproject.ui.theme.ArchiveatProjectTheme
+
+@Composable
+fun BackTopBar(
+    title: String = "",
+    onBack: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(ArchiveatProjectTheme.colors.white)
+            .height(45.dp)
+            .padding(horizontal = 20.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = onBack,
+            modifier = Modifier.size(20.dp)) {
+            Icon(
+                painter = painterResource( id = R.drawable.ic_arrow_back),
+                contentDescription = "back",
+                tint = ArchiveatProjectTheme.colors.gray800
+            )
+        }
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxHeight(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+        Spacer(modifier = Modifier.size(20.dp))
+    }
+}
+
+@Preview(showBackground = false)
+@Composable
+fun BackTopBarPrev() {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(24.dp)
+    ) {
+        BackTopBar(onBack = {})
+        BackTopBar(onBack = {}, title = "컴퓨터")
+    }
+}
---
+++ app/src/main/java/com/kuit/archiveatproject/presentation/newsletterdetails/screen/NewsletterDetailsAI.kt
@@ -0,0 +1,10 @@
+package com.kuit.archiveatproject.presentation.newsletterdetails.screen
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun  NewsletterDetailsAI(
+
+){
+
+}
