19e6c51c16988495237bcd0ebf98e852fbdfe9e2
AyoungSe0 <ayoungseo1005@gmail.com>
2026-01-13 00:40:01 +0900

    fix: HomeContentCard 이미지 배치 수정

--- app/src/main/java/com/kuit/archiveatproject/core/component/tag/TextTag.kt
+++ app/src/main/java/com/kuit/archiveatproject/core/component/tag/TextTag.kt
@@ -1,6 +1,5 @@
 package com.kuit.archiveatproject.core.component.tag
 
-import android.R.attr.text
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
