5968cb3b7da6b877e8296cde5b63e2c29e1ff32d
BearYeWon <bear100152@gmail.com>
2026-01-18 23:47:24 +0900

    feat: 온보딩 화면 초기 배치

---
+++ app/src/main/java/com/kuit/archiveatproject/presentation/onboarding/component/OnboardingIndicator.kt
@@ -0,0 +1,43 @@
+package com.kuit.archiveatproject.presentation.onboarding.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import com.kuit.archiveatproject.ui.theme.ArchiveatProjectTheme
+import com.kuit.archiveatproject.ui.theme.Gray200
+
+@Composable
+fun OnboardingIndicator(
+    totalPage: Int,
+    currentPage: Int
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center
+    ){
+        repeat(totalPage) { index ->
+            Box(
+                modifier = Modifier
+                    .size(8.dp)
+                    .clip(CircleShape)
+                    .background(
+                        if (index == currentPage) ArchiveatProjectTheme.colors.primary
+                        else ArchiveatProjectTheme.colors.gray200
+                    )
+            )
+            if (index != totalPage - 1) {
+                Spacer(Modifier.width(16.dp))
+            }
+        }
+    }
+}
