9a248293aaa14e1b295a91838edf3c8399a344ee
J1huns <eksdidqufrhr@daum.net>
2026-01-18 13:30:46 +0900

    feat: 타앱에서 자앱으로 공유하기 눌렀을 때 하단 UI 뜨게끔 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,6 +67,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.compose.foundation.layout)
     ksp(libs.androidx.room.compiler)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
