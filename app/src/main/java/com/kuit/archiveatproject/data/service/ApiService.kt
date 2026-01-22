cff605b3bc3c407fcb5d8b2cd36ed060c6c34bf4
AyoungSe0 <ayoungseo1005@gmail.com>
2026-01-22 14:52:50 +0900

    feat: add explore inbox api

--- app/src/main/java/com/kuit/archiveatproject/data/service/ApiService.kt
+++ app/src/main/java/com/kuit/archiveatproject/data/service/ApiService.kt
@@ -1,12 +1,16 @@
 package com.kuit.archiveatproject.data.service
 
-import com.kuit.archiveatproject.data.dto.response.ExploreResponseDto
+import com.kuit.archiveatproject.data.dto.response.explore.ExploreResponseDto
+import com.kuit.archiveatproject.data.dto.response.explore.ExploreInboxResponseDto
 import retrofit2.http.GET
 import com.kuit.archiveatproject.data.dto.response.report.ReportResponseDto
 
 interface ApiService {
     @GET("/explore")
     suspend fun getExplore(): ExploreResponseDto
+
+    @GET("/explore/inbox")
+    suspend fun getExploreInbox(): ExploreInboxResponseDto
 
     @GET("/report")
     suspend fun getReport(): ReportResponseDto
