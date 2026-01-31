40fa41e1abf79980c3020a0c235784546a5b7b20
BearYeWon <bear100152@gmail.com>
2026-01-31 17:02:37 +0900

    feat: 리포트-핵심소비현황 uiState 및 컴포넌트 추가

--- app/src/main/java/com/kuit/archiveatproject/presentation/report/screen/ReportStatusScreen.kt
+++ app/src/main/java/com/kuit/archiveatproject/presentation/report/screen/ReportStatusScreen.kt
@@ -1,2 +1,22 @@
 package com.kuit.archiveatproject.presentation.report.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.kuit.archiveatproject.presentation.report.model.ReportStatusViewModel
+import com.kuit.archiveatproject.presentation.report.model.ReportUiState
+
+@Composable
+fun ReportStatusScreen(
+    viewModel: ReportStatusViewModel = hiltViewModel(),
+    modifier: Modifier = Modifier
+){
+
+}
+
+fun ReportStatusContent(
+    uiState: ReportUiState,
+    modifier: Modifier
+){
+
+}
