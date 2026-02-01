c78a104bf0b124a1eb5f9f85e7b7c932fb7d9790
BearYeWon <bear100152@gmail.com>
2026-02-02 00:30:48 +0900

    feat: 소비 밸런스 좌표 컴포넌트 추가

--- app/src/main/java/com/kuit/archiveatproject/presentation/report/screen/ReportBalanceScreen.kt
+++ app/src/main/java/com/kuit/archiveatproject/presentation/report/screen/ReportBalanceScreen.kt
@@ -1,2 +1,37 @@
 package com.kuit.archiveatproject.presentation.report.screen
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.kuit.archiveatproject.presentation.report.model.ReportStatusViewModel
+import com.kuit.archiveatproject.presentation.report.model.ReportUiState
+
+@Composable
+fun ReportBalanceScreen(
+    viewModel: ReportStatusViewModel = hiltViewModel(),
+    modifier: Modifier = Modifier
+) {
+    val uiState by viewModel.uiState.collectAsState()
+
+    ReportBalanceContent(
+        uiState = uiState,
+        modifier = modifier
+    )
+}
+
+@Composable
+fun ReportBalanceContent(
+    uiState: ReportUiState,
+    modifier: Modifier = Modifier
+){
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ReportBalanceContentPreview() {
+
+}
