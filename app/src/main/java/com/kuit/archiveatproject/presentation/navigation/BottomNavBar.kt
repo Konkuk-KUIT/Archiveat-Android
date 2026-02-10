1be17ecb78a0c77753894c1b45efb15bd53288b5
BearYeWon <bear100152@gmail.com>
2026-02-10 16:37:12 +0900

    fix: 탐색 메인 스크롤 영역, 패딩 값 수정

--- app/src/main/java/com/kuit/archiveatproject/presentation/navigation/BottomNavBar.kt
+++ app/src/main/java/com/kuit/archiveatproject/presentation/navigation/BottomNavBar.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,9 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -31,12 +28,13 @@ fun BottomNavBar(
     visible: Boolean,
     tabs: EnumEntries<NavTab>,
     currentTab: NavTab?,
-    onItemSelected: (NavTab) -> Unit
+    onItemSelected: (NavTab) -> Unit,
+    modifier: Modifier = Modifier
 ) {
     if (!visible) return
 
     Box(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
             .shadow(
                 elevation = 0.5.dp,
