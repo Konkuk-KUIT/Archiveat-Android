6dadeddca388e2e66b2ccd670b08924df134615e
AyoungSe0 <ayoungseo1005@gmail.com>
2026-02-17 01:59:06 +0900

    fix: confirmExploreInboxAll() 호출 시점을 인박스 화면에서 나갔을 때로 수정

--- app/src/main/java/com/kuit/archiveatproject/presentation/inbox/screen/InboxScreen.kt
+++ app/src/main/java/com/kuit/archiveatproject/presentation/inbox/screen/InboxScreen.kt
@@ -1,5 +1,6 @@
 package com.kuit.archiveatproject.presentation.inbox.screen
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -13,9 +14,11 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -43,10 +46,16 @@ fun InboxScreen(
     viewModel: InboxViewModel = hiltViewModel(),
 ) {
     val uiState by viewModel.uiState.collectAsState()
+    var isLeaving by remember { mutableStateOf(false) }
 
-    LaunchedEffect(Unit) {
+    val onBackFromInbox = onBackFromInbox@{
+        if (isLeaving) return@onBackFromInbox
+        isLeaving = true
         viewModel.confirmExploreInboxAll()
+        onBackToExploreFirstDepth()
     }
+
+    BackHandler { onBackFromInbox() }
 
     Box(
         modifier = modifier
@@ -54,7 +63,7 @@ fun InboxScreen(
     ) {
         InboxScreenContent(
             inbox = uiState.inbox,
-            onBackToExploreFirstDepth = onBackToExploreFirstDepth,
+            onBackToExploreFirstDepth = onBackFromInbox,
             onDelete = { id -> viewModel.deleteNewsletter(id, onDelete) },
             onOpenOriginal = onOpenOriginal,
             onClickEdit = viewModel::openEditSheet
